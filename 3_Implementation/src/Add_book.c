1231ce88001090d57350c0c75cf203b0440b431b
Pavanv408 <101516120+Pavanv408@users.noreply.github.com>
2022-04-02 17:05:42 +0530

    Update Add_book.c

--- 3_Implementation/src/Add_book.c
+++ 3_Implementation/src/Add_book.c
@@ -5,9 +5,9 @@
  * 
  * @return test_values 
  */
-test_values enter_new_record(int id, char title[]){
+test_values enter_new_record(int id, char title[], char author[]){
     FILE *fptr;
-    fptr = fopen("library_data.txt","ab");
+    fptr = fopen("book_management.txt","ab");
     if(fptr==NULL){
         printf("Unable to open the file\n");
         return fail;
@@ -15,10 +15,15 @@ test_values enter_new_record(int id, char title[]){
         book_data *new_book = (book_data*)malloc(sizeof(book_data));
         new_book->book_id = id;
         strcpy(new_book->book_title, title);
+        strcpy(new_book->book_author, author);
         strcpy(new_book->status, "Available");
-        strcpy(new_book->member_first_name, "N/A");
-        strcpy(new_book->member_last_name, ".");
-        new_book->member_id=0;
+        strcpy(new_book->student_first_name, "N/A");
+        strcpy(new_book->student_last_name, ".");
+        new_book->student_usn=0;
+/**
+ * @brief the values for the student can be update in update_book
+ * 
+ */
         strcpy(new_book->date_of_issue,"N/A");
         strcpy(new_book->due_date,"N/A");
         fwrite(new_book, sizeof(book_data), 1, fptr);
