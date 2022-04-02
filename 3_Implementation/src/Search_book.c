4e424a5c131475c0feb249271f37454cbeaa8f14
Pavanv408 <101516120+Pavanv408@users.noreply.github.com>
2022-04-02 17:07:06 +0530

    Update Search_book.c

--- 3_Implementation/src/Search_book.c
+++ 3_Implementation/src/Search_book.c
@@ -8,7 +8,7 @@
  */
 test_values view_a_record(int id){
     FILE *fptr;
-    fptr = fopen("library_data.txt","rb");
+    fptr = fopen("book_management.txt","rb");
     if(fptr==NULL){
         printf("Unable to open the file\n");
         return fail;
@@ -17,12 +17,12 @@ test_values view_a_record(int id){
         while(fread(book_to_find, sizeof(book_data), 1, fptr)!='\0'){
             if(id==book_to_find->book_id){
                 printf("Book ID: %d\n", book_to_find->book_id);
-                printf("Title: %s\n", book_to_find->book_title);
+                printf("Title of the book: %s\n", book_to_find->book_title);
                 printf("Status: %s\n", book_to_find->status);
-                printf("Issued by: %s %s\n", book_to_find->member_first_name, book_to_find->member_last_name);
-                printf("ID of the member: %d\n", book_to_find->member_id);
-                printf("Date of issue: %s\n", book_to_find->date_of_issue);
-                printf("Due date: %s\n", book_to_find->due_date);
+                printf("Issued by: %s %s\n", book_to_find->student_first_name, book_to_find->student_last_name);
+                printf("USN of the student: %d\n", book_to_find->student_usn);
+                printf("Date of issue book: %s\n", book_to_find->date_of_issue);
+                printf("Due date of book: %s\n", book_to_find->due_date);
                 fclose(fptr);
                 free(book_to_find);
                 return pass;
