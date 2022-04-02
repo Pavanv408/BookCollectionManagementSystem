a237f55ad1f67c87077a06c58266eaaa4ef04758
Pavanv408 <101516120+Pavanv408@users.noreply.github.com>
2022-04-02 17:08:21 +0530

    Update view_book.c

--- 3_Implementation/src/view_book.c
+++ 3_Implementation/src/view_book.c
@@ -7,7 +7,7 @@
  */
 test_values view_all_records(){
     FILE *fptr;
-    fptr = fopen("library_data.txt","rb");
+    fptr = fopen("book_management.txt","rb");
     if(fptr==NULL){
         printf("Unable to open the file\n");
         return fail;
@@ -15,10 +15,12 @@ test_values view_all_records(){
         book_data *book_to_find=(book_data*)malloc(sizeof(book_data));
         while(fread(book_to_find, sizeof(book_data), 1, fptr)!='\0'){
             printf("Book ID: %d", book_to_find->book_id);
-            printf("  Title: %s\n\n", book_to_find->book_title);
+            printf("  Title of  the book: %s", book_to_find->book_title);
+            printf("  Author of the book: %s\n\n", book_to_find->book_author);
         }
         fclose(fptr);
         free(book_to_find);
         return pass;
     }   
 }
+
