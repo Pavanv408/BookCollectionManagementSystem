ab41f3f883d0cdb9ae7f4fd0f2f4dcfeb1786155
Pavanv408 <101516120+Pavanv408@users.noreply.github.com>
2022-04-02 17:07:43 +0530

    Update update_book.c

--- 3_Implementation/src/update_book.c
+++ 3_Implementation/src/update_book.c
@@ -7,10 +7,10 @@
  * @return test_values 
  */
 
-test_values update_record(int id, char new_status[], char new_date_of_issue[], char new_due_date[], char new_member_first_name[],char new_member_last_name[], int new_member_id){
+test_values update_record(int id, char new_status[], char new_date_of_issue[], char new_due_date[], char new_student_first_name[],char new_student_last_name[], int new_student_usn){
     FILE *fptr1;
     FILE *fptr2;
-    fptr1 = fopen("library_data.txt","rb");
+    fptr1 = fopen("book_management.txt","rb");
     fptr2 = fopen("temp_data.txt","wb");
     if(fptr1==NULL){
         printf("Unable to open the file\n");
@@ -22,17 +22,17 @@ test_values update_record(int id, char new_status[], char new_date_of_issue[], c
                 strcpy(update_status->status,new_status);
                 strcpy(update_status->date_of_issue,new_date_of_issue);
                 strcpy(update_status->due_date,new_due_date);
-                strcpy(update_status->member_first_name,new_member_first_name);
-                strcpy(update_status->member_last_name,new_member_last_name);
-                update_status->member_id=new_member_id;
+                strcpy(update_status->student_first_name,new_student_first_name);
+                strcpy(update_status->student_last_name,new_student_last_name);
+                update_status->student_usn=new_student_usn;
             }
             fwrite(update_status, sizeof(book_data), 1, fptr2);            
         }
         fclose(fptr1);
         fclose(fptr2);
         free(update_status);
-        remove("library_data.txt");
-        rename("temp_data.txt", "library_data.txt");
+        remove("book_management.txt");
+        rename("temp_data.txt", "book_management.txt");
     }
     return pass;   
 }
